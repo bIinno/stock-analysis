ac724de6e0b674437b2e594e127d18d8c90ee564
blinno <blinnobusiness@gmail.com>
2023-09-18 22:36:01 +0100

    updated

---
+++ get_ticker_data.py
@@ -0,0 +1,39 @@
+# TEST FILE TO CHECK ACCURACY OF MAIN.PY
+
+# Import necessary libraries
+import requests
+
+# Define constants and API key
+ALPHA_VANTAGE_API_KEY = "ME5NSWDBKBB2KK05"
+ALPHA_VANTAGE_API_URL = "https://www.alphavantage.co/query"
+ALPHA_VANTAGE_API_FUNCTION = "OVERVIEW"
+TICKER_SYMBOL = "TM"  # Replace with the desired ticker symbol
+
+# Function to get the JSON data for a specific ticker
+def get_ticker_data(ticker):
+    # Define parameters
+    params = {
+        "function": ALPHA_VANTAGE_API_FUNCTION,
+        "symbol": ticker,
+        "apikey": ALPHA_VANTAGE_API_KEY
+    }
+
+    # Make the request to AlphaVantage
+    response = requests.get(ALPHA_VANTAGE_API_URL, params=params)
+
+    # If the status code is 200, return the JSON data
+    if response.status_code == 200:
+        return response.json()
+    # Otherwise, print an error message
+    else:
+        print("Error fetching data from AlphaVantage")
+        return None
+
+# Fetch and print the JSON data for the specified ticker
+ticker_data = get_ticker_data(TICKER_SYMBOL)
+
+if ticker_data:
+    # Print the entire JSON dataset
+    print(ticker_data)
+else:
+    print(f"Data for {TICKER_SYMBOL} could not be retrieved.")
--- main.py
+++ main.py
@@ -1,35 +1,46 @@
+# Import necessary libraries
 import requests
 import time
 
+# Define constants and API key
 ALPHA_VANTAGE_API_KEY = "ME5NSWDBKBB2KK05"
 ALPHA_VANTAGE_API_URL = "https://www.alphavantage.co/query"
 ALPHA_VANTAGE_API_FUNCTION = "OVERVIEW"
-ALPHA_VANTAGE_API_TICKER_FILE = "crazy/ticker.txt"
+ALPHA_VANTAGE_API_TICKER_FILE = "ticker.txt"
 
+# Define custom thresholds for ratios
 thresholds = {
-    "PE Ratio": 5,
-    "PEG Ratio": 2,
-    "PriceToBookRatio": 0.7,
-    "EV to Revenue": 2,
+    "PE Ratio": 1,  # Customize your threshold value here
+    "PEG Ratio": 1,  # Customize your threshold value here
+    "PriceToBookRatio": 0.7,  # Customize your threshold value here
+    "EV to Revenue": 0.5,  # Customize your threshold value here for EV to Revenue
+    # Add more ratios and thresholds as needed
 }
 
+# Function to get the stock data from AlphaVantage
 def get_stock_data(ticker):
+    # Define parameters
     params = {
         "function": ALPHA_VANTAGE_API_FUNCTION,
         "symbol": ticker,
         "apikey": ALPHA_VANTAGE_API_KEY
     }
 
+    # Make the request to AlphaVantage
     response = requests.get(ALPHA_VANTAGE_API_URL, params=params)
 
+    # If the status code is 200, return the response
     if response.status_code == 200:
         return response.json()
+    # Otherwise, print an error message
     else:
-        print("Error fetching data from AlphaVantage")
+        print(f"Error fetching data for {ticker}: {response.status_code}")
         return None
 
+# Initialize the tickers variable as an empty list
 tickers = []
 
+# Read tickers from a text file with error handling
 try:
     with open(ALPHA_VANTAGE_API_TICKER_FILE, "r") as ticker_file:
         tickers = ticker_file.read().splitlines()
@@ -40,32 +51,37 @@ except PermissionError:
 except Exception as e:
     print(f"Error reading ticker file: {str(e)}")
 
+# Check if tickers were successfully loaded
 if not tickers:
     print("No tickers found. Check the ticker file contents.")
 
+# Create a list to store passed stocks
 passed_stocks = []
 
+# Loop through the tickers and fetch data for each
 for i, ticker in enumerate(tickers, start=1):
     try:
         dataset = get_stock_data(ticker)
     except:
         print(f"Error fetching data for {ticker}")
-    else:
-        print(f"Fetched data for {ticker}")
+        continue
 
+    # Access and print specific parameters and check against thresholds
     if dataset:
         symbol = dataset.get('Symbol', 'N/A')
         sector = dataset.get('Sector', 'N/A')
-        pe_ratio = float(dataset.get('PERatio', '0'))
-        peg_ratio = float(dataset.get('PEGRatio', '0'))
-        price_to_book_ratio = float(dataset.get('PriceToBookRatio', '0'))
-        ev_to_revenue = float(dataset.get('EVToRevenue', '0'))
+        pe_ratio = dataset.get('PERatio', 'N/A')
+        peg_ratio = dataset.get('PEGRatio', 'N/A')
+        price_to_book_ratio = dataset.get('PriceToBookRatio', 'N/A')
+        ev_to_revenue = dataset.get('EVToRevenue', 'N/A')
 
         if (
-            pe_ratio >= thresholds.get('PE Ratio', float('-inf')) and
-            peg_ratio >= thresholds.get('PEG Ratio', float('-inf')) and
-            price_to_book_ratio >= thresholds.get('PriceToBookRatio', float('-inf')) and
-            ev_to_revenue >= thresholds.get('EV to Revenue', float('-inf'))
+            pe_ratio != 'N/A' and peg_ratio != 'N/A' and price_to_book_ratio != 'N/A' and ev_to_revenue != 'N/A' and
+            float(pe_ratio) >= thresholds.get('PE Ratio', float('-inf')) and
+            float(peg_ratio) >= thresholds.get('PEG Ratio', float('-inf')) and
+            float(price_to_book_ratio) >= thresholds.get('PriceToBookRatio', float('-inf')) and
+            float(ev_to_revenue) >= thresholds.get('EV to Revenue', float('-inf'))
+            # Add more ratio checks here
         ):
             print(f"{ticker} meets the threshold criteria.")
             passed_stocks.append((symbol, sector, pe_ratio, peg_ratio, price_to_book_ratio, ev_to_revenue))
@@ -76,6 +92,7 @@ for i, ticker in enumerate(tickers, start=1):
         print(f"Waiting for 60 seconds to account for API rate limit...")
         time.sleep(60)
 
+# Export passed stocks to an output file
 output_file = "output.txt"
 with open(output_file, "w") as f:
     f.write("Symbol, Sector, PE Ratio, PEG Ratio, Price-to-Book Ratio, EV to Revenue\n")
